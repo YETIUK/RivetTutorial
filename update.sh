28878c1bf36ccf985c322b01c9f30ac926447e33
YETIUK <yetidev@yetiuk.com>
2016-01-13 08:07:08 -0500

    fixing update.sh

--- update.sh
+++ update.sh
@@ -9,14 +9,15 @@ cd -
 
 rm /home/rivet/Rivet/boost_1_56_0.tar.gz
 sudo apt-get clean -y
-sudo apt-get install texlive-base -y
-sudo apt-get install texlive-latex-extra -y
-sudo apt-get install texlive-latex-extra -y
+sudo apt-get install --no-install-recommends texlive-base -y
+sudo apt-get install --no-install-recommends texlive-latex-extra -y
+sudo apt-get install --no-install-recommends texlive-latex-extra -y
 sudo apt-get install --no-install-recommends ps2eps -y
-sudo apt-get install --no-install-recommends texlive-extras-utils -y
+sudo apt-get install --no-install-recommends texlive-extra-utils -y
 sudo apt-get install --no-install-recommends texlive-font-utils -y
 sudo apt-get install --no-install-recommends texlive-pstricks -y
 sudo apt-get install --no-install-recommends texlive-latex-extra -y
+sudo apt-get install --no-install-recommends texlive-fonts-recommended -y
 sudo apt-get install imagemagick -y
 
 wget https://users.hepforge.org/~holsch/7TeVZJets_short.hepmc.bz2
